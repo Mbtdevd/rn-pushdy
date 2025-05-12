c0f2d6979a02dd1f67fd961fc9a3ddbb19d4d2cf
tanpm <tanpm@mobiletech.vn>
2025-05-12 15:37:43 +0700

    base: make it work

--- android/src/main/java/com/rnpushdy/RnPushdyModule.kt
+++ android/src/main/java/com/rnpushdy/RnPushdyModule.kt
@@ -1,6 +1,9 @@
 package com.rnpushdy
 
+import com.facebook.react.bridge.Promise
 import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.bridge.ReadableArray
+import com.facebook.react.bridge.ReadableMap
 import com.facebook.react.module.annotations.ReactModule
 
 @ReactModule(name = RnPushdyModule.NAME)
@@ -15,6 +18,169 @@ class RnPushdyModule(reactContext: ReactApplicationContext) :
   // See https://reactnative.dev/docs/native-modules-android
   override fun multiply(a: Double, b: Double): Double {
     return a * b
+  }
+
+  override fun sampleMethod(stringArgument: String?, numberArgument: Double, promise: Promise?) {
+    TODO("Not yet implemented")
+  }
+
+  override fun makeCrash(promise: Promise?) {
+    TODO("Not yet implemented")
+  }
+
+  override fun initPushdy(options: ReadableMap?, promise: Promise?) {
+    TODO("Not yet implemented")
+  }
+
+  override fun isRemoteNotificationRegistered(promise: Promise?) {
+    TODO("Not yet implemented")
+  }
+
+  override fun isAppOpenedFromPush(promise: Promise?) {
+    TODO("Not yet implemented")
+  }
+
+  override fun isNotificationEnabled(promise: Promise?) {
+    TODO("Not yet implemented")
+  }
+
+  override fun startHandleIncommingNotification(promise: Promise?) {
+    TODO("Not yet implemented")
+  }
+
+  override fun stopHandleIncommingNotification(promise: Promise?) {
+    TODO("Not yet implemented")
+  }
+
+  override fun getReadyForHandlingNotification(promise: Promise?) {
+    TODO("Not yet implemented")
+  }
+
+  override fun setPushBannerAutoDismiss(autoDismiss: Boolean, promise: Promise?) {
+    TODO("Not yet implemented")
+  }
+
+  override fun setPushBannerDismissDuration(sec: Double, promise: Promise?) {
+    TODO("Not yet implemented")
+  }
+
+  override fun setCustomPushBanner(viewType: String?, promise: Promise?) {
+    TODO("Not yet implemented")
+  }
+
+  override fun useSDKHandler(enabled: Boolean, promise: Promise?) {
+    TODO("Not yet implemented")
+  }
+
+  override fun handleCustomInAppBannerPressed(notificationId: String?, promise: Promise?) {
+    TODO("Not yet implemented")
+  }
+
+  override fun setCustomMediaKey(mediaKey: String?, promise: Promise?) {
+    TODO("Not yet implemented")
+  }
+
+  override fun setDeviceId(deviceId: String?, promise: Promise?) {
+    TODO("Not yet implemented")
+  }
+
+  override fun getDeviceId(promise: Promise?) {
+    TODO("Not yet implemented")
+  }
+
+  override fun getDeviceToken(promise: Promise?) {
+    TODO("Not yet implemented")
+  }
+
+  override fun getPendingNotification(promise: Promise?) {
+    TODO("Not yet implemented")
+  }
+
+  override fun getPendingNotifications(promise: Promise?) {
+    TODO("Not yet implemented")
+  }
+
+  override fun getInitialNotification(promise: Promise?) {
+    TODO("Not yet implemented")
+  }
+
+  override fun removeInitialNotification(promise: Promise?) {
+    TODO("Not yet implemented")
+  }
+
+  override fun setAttributeFromValueContainer(
+    attr: String?,
+    valueContainer: ReadableMap?,
+    commitImmediately: Boolean,
+    promise: Promise?
+  ) {
+    TODO("Not yet implemented")
+  }
+
+  override fun pushAttributeArray(
+    attr: String?,
+    value: ReadableArray?,
+    commitImmediately: Boolean,
+    promise: Promise?
+  ) {
+    TODO("Not yet implemented")
+  }
+
+  override fun getPlayerID(promise: Promise?) {
+    TODO("Not yet implemented")
+  }
+
+  override fun setBadgeOnForeground(enable: Boolean, promise: Promise?) {
+    TODO("Not yet implemented")
+  }
+
+  override fun setSubscribedEvents(subscribedEventNames: ReadableArray?, promise: Promise?) {
+    TODO("Not yet implemented")
+  }
+
+  override fun getPendingEvents(count: Double, promise: Promise?) {
+    TODO("Not yet implemented")
+  }
+
+  override fun setPendingEvents(events: ReadableArray?, promise: Promise?) {
+    TODO("Not yet implemented")
+  }
+
+  override fun setApplicationId(applicationId: String?, promise: Promise?) {
+    TODO("Not yet implemented")
+  }
+
+  override fun removePendingEvents(count: Double, promise: Promise?) {
+    TODO("Not yet implemented")
+  }
+
+  override fun trackEvent(
+    eventName: String?,
+    eventProperties: ReadableMap?,
+    immediate: Boolean,
+    promise: Promise?
+  ) {
+    TODO("Not yet implemented")
+  }
+
+  override fun pushPendingEvents(promise: Promise?) {
+    TODO("Not yet implemented")
+  }
+
+  override fun subscribe(promise: Promise?) {
+    TODO("Not yet implemented")
+  }
+
+  override fun getAllBanners(promise: Promise?) {
+    TODO("Not yet implemented")
+  }
+
+  override fun trackBanner(bannerId: String?, type: String?, promise: Promise?) {
+    TODO("Not yet implemented")
+  }
+
+  override fun getBannerData(bannerId: String?, promise: Promise?) {
+    TODO("Not yet implemented")
   }
 
   companion object {
--- src/NativeRnPushdy.ts
+++ src/NativeRnPushdy.ts
@@ -3,6 +3,75 @@ import { TurboModuleRegistry } from 'react-native';
 
 export interface Spec extends TurboModule {
   multiply(a: number, b: number): number;
+
+  sampleMethod(
+    stringArgument: string,
+    numberArgument: number
+  ): Promise<(string | number)[]>;
+
+  makeCrash(): Promise<void>;
+
+  initPushdy(options: { [key: string]: string }): Promise<boolean>;
+  isRemoteNotificationRegistered(): Promise<boolean>;
+  isAppOpenedFromPush(): Promise<boolean>;
+  isNotificationEnabled(): Promise<boolean>;
+
+  startHandleIncommingNotification(): Promise<boolean>;
+  stopHandleIncommingNotification(): Promise<boolean>;
+  getReadyForHandlingNotification(): Promise<boolean>;
+
+  setPushBannerAutoDismiss(autoDismiss: boolean): Promise<boolean>;
+  setPushBannerDismissDuration(sec: number): Promise<boolean>;
+
+  setCustomPushBanner(viewType: string): Promise<boolean>;
+  useSDKHandler(enabled: boolean): Promise<boolean>;
+  handleCustomInAppBannerPressed(notificationId: string): Promise<boolean>;
+
+  setCustomMediaKey(mediaKey: string): Promise<boolean>;
+  setDeviceId(deviceId: string): Promise<boolean>;
+  getDeviceId(): Promise<string>;
+  getDeviceToken(): Promise<string>;
+
+  getPendingNotification(): Promise<{ [key: string]: string }>;
+  getPendingNotifications(): Promise<Array<{ [key: string]: string }>>;
+  getInitialNotification(): Promise<{ [key: string]: string }>;
+  removeInitialNotification(): Promise<boolean>;
+
+  setAttributeFromValueContainer(
+    attr: string,
+    valueContainer: { data: string | number | boolean },
+    commitImmediately: boolean
+  ): Promise<boolean>;
+
+  pushAttributeArray(
+    attr: string,
+    value: Array<string | number | boolean>,
+    commitImmediately: boolean
+  ): Promise<boolean>;
+
+  getPlayerID(): Promise<string>;
+
+  setBadgeOnForeground(enable: boolean): Promise<boolean>;
+  setSubscribedEvents(subscribedEventNames: string[]): Promise<boolean>;
+
+  getPendingEvents(count: number): Promise<Array<{ [key: string]: string }>>;
+  setPendingEvents(events: Array<{ [key: string]: string }>): Promise<boolean>;
+
+  setApplicationId(applicationId: string): Promise<boolean>;
+  removePendingEvents(count: number): Promise<boolean>;
+
+  trackEvent(
+    eventName: string,
+    eventProperties: { [key: string]: string | number | boolean },
+    immediate: boolean
+  ): Promise<boolean>;
+
+  pushPendingEvents(): Promise<boolean>;
+  subscribe(): Promise<boolean>;
+
+  getAllBanners(): Promise<Array<{ [key: string]: string }>>;
+  trackBanner(bannerId: string, type: string): Promise<boolean>;
+  getBannerData(bannerId: string): Promise<{ [key: string]: string }>;
 }
 
 export default TurboModuleRegistry.getEnforcing<Spec>('RnPushdy');
